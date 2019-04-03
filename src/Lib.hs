877b688524e4b52d6553ce8d54fc688cece2e19d
Akihito KIRISAKI <endouryusuke@realglobe.jp>
2019-04-03 11:23:33 +0900

    Show log

--- src/Lib.hs
+++ src/Lib.hs
@@ -29,14 +29,14 @@ data Exp
   | Val Exp
 
 instance Show Exp where
-  show (Bool v) = show v
+  show (Bool v) = "(" ++ show v ++ ")"
   show (Func _) =  "(Func)"
   show (Func2 _) = "(Func2)"
   show (Func3 _) = "(Func3)"
-  show Eff = "Eff"
-  show (Apply x y) = "Apply " ++ show x ++ " " ++ show y
-  show Nil = "Nil"
-  show (Val _) = "Val"
+  show Eff = "(Eff)"
+  show (Apply x y) = "(Apply) " ++ show x ++ " " ++ show y
+  show Nil = "(Nil)"
+  show (Val _) = "(Val)"
 
 eval :: Func
 eval Nil= pure Nil
@@ -46,16 +46,15 @@ eval v@(Func2 _) = unsafePerformIO $ putStrLn "> Func2" >> (pure $ pure v)
 eval v@(Func3 _) = unsafePerformIO $ putStrLn "> Func3" >> (pure $ pure v)
 eval (Val exp) = unsafePerformIO $ putStrLn "> Val" >> (pure $ eval exp)
 eval Eff = tell (Sum 1) >> (unsafePerformIO $ putStrLn "> Eff" >> (pure $ pure Nil))
-eval (Apply (Func f) x) = unsafePerformIO $ putStrLn ("> Apply Func1 " ++ show x) >> (pure $ eval x >>= f)
-eval (Apply (Func2 f) x) = unsafePerformIO $ putStrLn ("> Apply Func2 " ++ show x) >> (pure $ eval . Func $ f x)
-eval (Apply (Func3 f) x) = unsafePerformIO $ putStrLn ("> Apply Func3 " ++ show x) >> (pure $ eval . Func2 $ f x)
+eval (Apply (Func f) x) = unsafePerformIO $ putStrLn ("> Apply Func1 " ++ show x) >> (pure $ f x >>= eval)
+eval (Apply (Func2 f) x) = unsafePerformIO $ putStrLn ("> Apply Func2 " ++ show x) >> (pure $ pure . Func $ f x)
+eval (Apply (Func3 f) x) = unsafePerformIO $ putStrLn ("> Apply Func3 " ++ show x) >> (pure $ pure . Func2 $ f x)
 eval (Apply exp@(Apply _ _) x) = do
-  unsafePerformIO $ putStrLn ("> Apply Apply " ++ show exp ++ " " ++ show x) >> (pure $ pure ())
-  e <- eval exp
+  e <-   unsafePerformIO $ putStrLn ("> Apply Apply " ++ show exp ++ " " ++ show x) >> (pure $ eval exp)
   eval $ Apply e x
 eval (Apply x y) = pure . unsafePerformIO $ putStrLn ("failed: " ++ show x ++ " " ++ show y) >> pure Nil
 
 if_ :: Func3
-if_ (Bool cond) x y = if cond then eval x else eval y
+if_ (Bool cond) x y = if cond then pure x else pure y
 
 tree = Apply (Apply (Apply (Func3 if_) (Bool False)) (Val Eff)) Nil
